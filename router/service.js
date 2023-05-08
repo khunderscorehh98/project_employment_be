51de8c585f3f2a0d092d0f589706a6da265143e4
haziqimac <hpprojects00@gmail.com>
2023-05-08 11:28:27 +0800

    service get and post; todo: delete and put

---
+++ router/service.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const router = express.Router()
+
+const db = require('../config/db')
+
+// Get All
+router.get('/service', (req, res) => {
+    let sql = `select * from service`
+    db.connection.query(sql, (error, result) => {
+        if (error) {
+            res.status(500).json({
+                error: true,
+                message: error.message
+            })
+        }
+        res.status(200).json({
+            error: false,
+            message: 'Here is the record request!',
+            data: result
+        })
+    })
+})
+
+// Get by ID
+router.get('/service/:id', (req, res) => {
+    let id = req.params.id
+    let sql = `select * from services where id = ${id}`
+
+    db.connection.query(sql, (error, result) => {
+        if (error) {
+            res.status(500).json({
+                error: true,
+                message: error.message
+            })
+        }
+        res.status(200).json({
+            error: false,
+            message: 'Here is the specific record that you requested!',
+            data: result
+        })
+    },
+    )
+})
+
+//POST
+router.post('/service', (req, res) => {
+    let wrap = req.body
+
+    let service = wrap.service
+    let price = wrap.price
+    let category = wrap.category
+    let description = wrap.description
+    let user_id = uuid.v4()
+
+    let sql = `insert into service (service, price, category, description, user_id) values ('${service}', '${price}', '${category}', '${description}', ${user_id})`
+
+    db.connection.query(sql, (error, result) => {
+        if(error) {
+            res.status(500).json({
+                error: true,
+                message: error.message
+            })
+        }
+        res.status(201).json({
+            error: false,
+            message: 'Record has been added!',
+            data: result
+        })
+    }
+})
+
+//PUT
+router.put()
+
+//DELETE
+router.delete()
+module.exports = router
